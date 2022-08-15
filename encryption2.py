f9c52c6f1fd03f2de34994c82ecf63f06c1aad1b
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2022-08-15 12:03:28 +0800

    Update encryption2.py

--- encryption2.py
+++ encryption2.py
@@ -49,7 +49,7 @@ ceasar()
 # This is the link to this Python Coding Challenge
 # https://www.codewars.com/kata/526d42b6526963598d0004db/train/python
 # alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
-# def ceasar(start_text = input('Type in any word:\n'), shift_amount = int(input('Type a number from 1 to 26:\n')), cipher_direction  = input('Type encode or decode:\n')):
+# def ceasar(start_text = input('Type in any word:\n').lower(), shift_amount = int(input('Type a number from 1 to 26:\n')), cipher_direction  = input('Type encode or decode:\n')):
 #     str = ''
 #     for i in start_text:
 #         print(i)
